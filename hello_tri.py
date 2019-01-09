dd444d7df4c5dfa2eb8262a8c5998372f2ca356d
00KB42 <00KB42@gmail.com>
2019-01-09 14:24:17 -0800

    Version 2

--- hello_tri.py
+++ hello_tri.py
@@ -1,3 +1,5 @@
+# Prompts user for height of triangle be created and then prints "hello world!" (normal sequence + reversed)
+
 height = int(input("Enter a height for your triangle: "))
 str = "hello world!"
 i = 0
@@ -10,10 +12,9 @@ while i is not (height - 1):
     i += 1
     space_amt += 2
     tab -= 1
-print(" " * tab, "/_", "_" * (space_amt - 2), "_\\\n")
+print(" " * tab, "/_", "_ " * ((space_amt - 1)//2), "_\\\n")
 print(" " * (height//2), str[::-1])
 print(" " * (height//2), str)
-
 
 
 
